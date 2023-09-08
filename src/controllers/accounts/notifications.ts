d8e7f647721fbf6361ce7cf5c65dfeeabf18021a
SerenaYaoo <yucheny3@andrew.cmu.edu>
2023-09-07 22:00:24 -0400

    attempt recalled

--- src/controllers/accounts/notifications.ts
+++ src/controllers/accounts/notifications.ts
@@ -87,8 +87,6 @@ const notificationsController: NotificationsController = {
 
 
         if ('filter' in selectedFilter) {
-            // The next line calls a function in a module that has not been updated to TS yet
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
             nids = await user.notifications.getAll(req.uid, selectedFilter.filter);
         } else {
             return;
